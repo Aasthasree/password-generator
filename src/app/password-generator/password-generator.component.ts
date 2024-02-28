253375319c7531f9c8d0db7ec2a57c9c4a527abd
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-28 17:46:38 +0530

    feat(password-generator): install and integrate @angular-slider/ngx-slider

--- src/app/password-generator/password-generator.component.ts
+++ src/app/password-generator/password-generator.component.ts
@@ -1,12 +1,18 @@
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-password-generator',
   standalone: true,
-  imports: [],
+  imports: [NgxSliderModule],
   templateUrl: './password-generator.component.html',
   styleUrl: './password-generator.component.scss'
 })
 export class PasswordGeneratorComponent {
+  value: number = 100;
+  options: Options = {
+    floor: 0,
+    ceil: 200
+  };
 
 }
