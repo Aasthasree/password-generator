e309064f0103539cedbbb18e5c286ef5b574a5ac
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-29 15:54:02 +0530

    feat(password-generator): implement password generation logic

--- src/app/password-generator/password-generator.component.ts
+++ src/app/password-generator/password-generator.component.ts
@@ -1,5 +1,6 @@
 //Angular Imports
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 //Third-party
 import { PasswordStrengthMeterComponent } from 'angular-password-strength-meter';
 import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
@@ -7,7 +8,7 @@ import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
 @Component({
   selector: 'app-password-generator',
   standalone: true,
-  imports: [NgxSliderModule, PasswordStrengthMeterComponent],
+  imports: [NgxSliderModule, PasswordStrengthMeterComponent, FormsModule],
   templateUrl: './password-generator.component.html',
   styleUrl: './password-generator.component.scss'
 })
@@ -19,4 +20,33 @@ export class PasswordGeneratorComponent {
   };
 
   password: string = '';
+  includeUppercaseLetters: boolean = true;
+  includeLowercaseLetters: boolean = true;
+  includeNumbersLetters: boolean = true;
+  includeSymbolsLetters: boolean = true;
+
+  generatePassword() {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let availableCharacters = '';
+
+    if (this.includeUppercaseLetters) availableCharacters += uppercaseChars;
+    if (this.includeLowercaseLetters) availableCharacters += lowercaseChars;
+    if (this.includeNumbersLetters) availableCharacters += numberChars;
+    if (this.includeSymbolsLetters) availableCharacters += symbolChars;
+
+    let generatePassword = '';
+
+    for (let i = 0; i < this.value; i++) {
+      const maxValue = availableCharacters.length;
+      const randomIndex = Math.floor(Math.random() * maxValue);
+      generatePassword += availableCharacters.charAt(randomIndex);
+    }
+
+    this.password = generatePassword;
+
+  }
 }
